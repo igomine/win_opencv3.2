a6cde2dcffe70e52f8bc36418baa55dc6239c40c
budd <igomine@126.com>
2017-02-16 17:21:08 +0800

    gao tcp to idec

--- raspberrypi/TCP_to_IDEC_0205.py
+++ raspberrypi/TCP_to_IDEC_0205.py
@@ -1,53 +1,167 @@
-import socket
+from pymodbus3.client.sync import ModbusTcpClient as ModbusClient
+from pymodbus3.transaction import ModbusSocketFramer as ModbusFramer
+import threading
+import logging
 import time
-import sys
-import struct
+import paho.mqtt.client as mqtt
+import json
 
-IDEC_COMMAND_WRITE_DO_4BYTE = [0, 0, 0, 0, 0, 0x0B, 1, 0x0F, 0, 0, 0, 0x20, 4, 0, 0, 0, 0]
-IDEC_RETURN_WRITE_DO = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-IDEC_COMMAND_READ_DI_40CHNEL = [0, 0, 0, 0, 0, 6, 1, 2, 0, 0, 0, 0x28]
-IDEC_RETURN_READ_DI = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+# <editor-fold desc="mqtt callback">
+def on_fpq_do_hx_message(client, userdata, msg):
+    print("msg:" + str(msg.payload))
+    # rsv_data = int(msg.payload)
+    # print(rsv_data)
+    if int(msg.payload) == 1:
+        rq = modbus_client.write_coils(0, [True] * 1, unit=0x01)
+    elif int(msg.payload) == 0:
+        rq = modbus_client.write_coils(0, [False] * 1, unit=0x01)
 
-SERVER_IP = "192.168.1.104"
-SERVER_PORT = 7654
 
-for d in range(1, 10):
-    print("%x" % d, end=" ")
+def on_fpq_do_sbf_message(client, userdata, msg):
+    if msg.payload == "1":
+        rq = modbus_client.write_coils(1, [True] * 1, unit=0x01)
+    elif msg.payload == "0":
+        rq = modbus_client.write_coils(1, [False] * 1, unit=0x01)
 
-print("Starting socket: TCP...")
-server_addr = (SERVER_IP, SERVER_PORT)
-socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+def on_message(client, userdata, msg):
+    print("topic:" + msg.topic + " Message:" + str(msg.payload) + "with QoS" + str(msg.qos))
+
+
+def on_publish(msg, rc):  # 成功发布消息的操作
+    if rc == 0:
+        print("publish success, msg = " + msg)
+
+
+def on_connect(client, userdata, flags, rc):  # 连接后的操作 0为成功
+    if rc == 0:
+        print("Connection successful")
+        client.subscribe("one2one/fpq/#", qos=1)  # qos
+    else:
+        print("Error:Connection returned " + str(rc))
+# </editor-fold>
+
+
+mqtt_client = mqtt.Client(
+    client_id="paho-fpq-client",  # 用来标识设备的ID，用户可自己定义，在同一个实例下，每个实体设备需要有一个唯一的ID
+    clean_session=True,
+    userdata=None,
+    protocol='MQTTv311'
+)
+
+# trust = "E:\root_cert.pem"  # 开启TLS时的认证文件目录
+user = "ds11-01/fpq"
+pwd = "Zw7VEoAXBwl+YxZm+Kw81cU9cMzMk4k4m+C4FgTvsXM="
+endpoint = "ds11-01.mqtt.iot.gz.baidubce.com"
+port = 1883
+topic = "one2one/fpq"
+
+# 防喷器数字量输出 回调
+mqtt_client.message_callback_add("one2one/fpq/hx", on_fpq_do_hx_message)
+mqtt_client.message_callback_add("one2one/fpq/sbf", on_fpq_do_sbf_message)
+
+# client.tls_insecure_set(True)  # 检查hostname的cert认证
+# client.tls_set(trust)  # 设置认证文件
+# mqtt_client.username_pw_set(user, pwd)  # 设置用户名，密码
+# mqtt_client.connect(endpoint, port, 60)  # 连接服务 keepalive=60
+# mqtt_client.on_connect = on_connect  # 连接后的操作
+# mqtt_client.on_message = on_message
+# mqtt_client.loop_start()
+# time.sleep(2)
+
+received_discrete_inputs_data = [False]*8
+
+
+def compare_update_di_list(old_list, new_list):
+    for i in range(0, len(new_list)):
+        if old_list[i] == new_list[i]:
+            pass
+        else:  # 两个列表对应元素不同，则输出对应的索引
+            print("update bytes[%d];" %i +"value=" + str(new_list[i]))
+    old_list = new_list
+
+
+class ModbusTcpToIDEC:
+    def __init__(self, devname, di_channel):
+        self.lastval = [False]*di_channel
+        self.devname = devname
+
+    def poll(self):
+        try:
+            rr = modbus_client.read_discrete_inputs(0, 8, unit=0x01)
+            for i in range(0, len(rr.bits)):
+                if self.lastval[i] == rr.bits[i]:
+                    pass
+                else:
+                    print("update bytes[%d];" % i + "value=" + str(rr.bits[i]))
+                    # send msg to mqtt broker
+                    fulltopic = "one2one/" + self.devname
+            self.lastval = rr.bits
+        except Exception as exc:
+            print("Error reading " + ": %s", exc)
+
+
+def modbus_rr():
+    # while threads_switch:
+    rr = modbus_client.read_discrete_inputs(0, 8, unit=0x01)
+    global received_discrete_inputs_data
+    compare_update_di_list(received_discrete_inputs_data, rr.bits)
+    # print("list response:" + str(rr.bits))
+    # print(int(rr.bits[2]))
+
+
+# global read_discrete_inputs_data
+# read_discrete_inputs_data = rr.bits
+#  modbus_client.write_coils(0, write_coils_data, unit=0x01)
+
+
+def modbus_print():
+    global threads_switch
+    while threads_switch:
+        global read_discrete_inputs_data
+        global discrete_inputs_data
+        if discrete_inputs_data != read_discrete_inputs_data:
+            msg = str(read_discrete_inputs_data)
+            rc, mid = mqtt_client.publish("one2one/fpq", payload=msg, qos=1)  # qos
+            on_publish(msg, rc)
+            print(read_discrete_inputs_data)
+            discrete_inputs_data = read_discrete_inputs_data
+
+
+logging.basicConfig()
+log = logging.getLogger()
+log.setLevel(logging.CRITICAL)
+
+modbus_client = ModbusClient('192.168.1.133', port=502, retries=2, framer=ModbusFramer)
+# modbus_client.close()
+modbus_client.connect()
+
+write_coils_data = []
+
+discrete_inputs_data = []
+threads_switch = 1
+idecrr = ModbusTcpToIDEC("fpq", 8)
 while True:
-    try:
-        print("Connecting to server @ %s:%d..." % (SERVER_IP, SERVER_PORT))
-        socket_tcp.connect(server_addr)
-        break
-    except Exception:
-        print("Can't connect to server, try it latter!")
-        time.sleep(1)
-        continue
-for test in range(10, 20):
-    print('%x' % test, end=" ")
-print("DO output")
-print("祝各位身体健康", end=' ')
-print("！")
+    idecrr.poll()
+    # time.sleep(1)
 
-while True:
-    try:
-        send_date = struct.pack("%dB" % (len(IDEC_COMMAND_WRITE_DO_4BYTE)), *IDEC_COMMAND_WRITE_DO_4BYTE)
-        socket_tcp.send(send_date)
-        rcv_data = socket_tcp.recv(512)
-        if len(rcv_data) > 0:
-            # unpack_rcv_data = struct.unpack("%dB" % len(rcv_data), rcv_data)
-            for i in rcv_data:
-                print("3")
-                print("0x%x" % i, end=" ")
-        # time.sleep(1)
-        continue
-    except Exception:
-        print("error:tcp closed")
-        socket_tcp.close()
-        socket_tcp = None
-        sys.exit(1)
+
+#
+# threads = []
+# t1 = threading.Thread(target=modbus_rr)
+# threads.append(t1)
+# t2 = threading.Thread(target=modbus_print)
+# threads.append(t2)
+#
+# for t in threads:
+#     t.setDaemon(True)
+#     t.start()
+#
+# for i in range(60):
+#     time.sleep(1)
+#     print(i)
+#
+# threads_switch = 0
+# modbus_client.close()
+
--- raspberrypi/pymodbustcp_test.py
+++ raspberrypi/pymodbustcp_test.py
@@ -4,7 +4,94 @@ import threading
 import logging
 import time
 import paho.mqtt.client as mqtt
-import datetime
+import json
+
+
+# <editor-fold desc="mqtt callback">
+def on_fpq_do_hx_message(client, userdata, msg):
+    print("msg:" + str(msg.payload))
+    # rsv_data = int(msg.payload)
+    # print(rsv_data)
+    if int(msg.payload) == 1:
+        rq = modbus_client.write_coils(0, [True] * 1, unit=0x01)
+    elif int(msg.payload) == 0:
+        rq = modbus_client.write_coils(0, [False] * 1, unit=0x01)
+
+
+def on_fpq_do_sbf_message(client, userdata, msg):
+    if msg.payload == "1":
+        rq = modbus_client.write_coils(1, [True] * 1, unit=0x01)
+    elif msg.payload == "0":
+        rq = modbus_client.write_coils(1, [False] * 1, unit=0x01)
+
+
+def on_message(client, userdata, msg):
+    print("topic:" + msg.topic + " Message:" + str(msg.payload) + "with QoS" + str(msg.qos))
+
+
+def on_publish(msg, rc):  # 成功发布消息的操作
+    if rc == 0:
+        print("publish success, msg = " + msg)
+
+
+def on_connect(client, userdata, flags, rc):  # 连接后的操作 0为成功
+    if rc == 0:
+        print("Connection successful")
+        client.subscribe("one2one/fpq/#", qos=1)  # qos
+    else:
+        print("Error:Connection returned " + str(rc))
+# </editor-fold>
+
+
+mqtt_client = mqtt.Client(
+    client_id="paho-fpq-client",  # 用来标识设备的ID，用户可自己定义，在同一个实例下，每个实体设备需要有一个唯一的ID
+    clean_session=True,
+    userdata=None,
+    protocol='MQTTv311'
+)
+
+# trust = "E:\root_cert.pem"  # 开启TLS时的认证文件目录
+user = "ds11-01/fpq"
+pwd = "Zw7VEoAXBwl+YxZm+Kw81cU9cMzMk4k4m+C4FgTvsXM="
+endpoint = "ds11-01.mqtt.iot.gz.baidubce.com"
+port = 1883
+topic = "one2one/fpq"
+
+# 防喷器数字量输出 回调
+mqtt_client.message_callback_add("one2one/fpq/hx", on_fpq_do_hx_message)
+mqtt_client.message_callback_add("one2one/fpq/sbf", on_fpq_do_sbf_message)
+
+# client.tls_insecure_set(True)  # 检查hostname的cert认证
+# client.tls_set(trust)  # 设置认证文件
+mqtt_client.username_pw_set(user, pwd)  # 设置用户名，密码
+mqtt_client.connect(endpoint, port, 60)  # 连接服务 keepalive=60
+mqtt_client.on_connect = on_connect  # 连接后的操作
+mqtt_client.on_message = on_message
+mqtt_client.loop_start()
+time.sleep(2)
+
+
+def modbus_rr():
+    global threads_switch
+    while threads_switch:
+        rr = modbus_client.read_discrete_inputs(0, 8, unit=0x01)
+        global read_discrete_inputs_data
+        read_discrete_inputs_data = rr.bits
+        # modbus_client.write_coils(0, write_coils_data, unit=0x01)
+
+
+def modbus_print():
+    global threads_switch
+    while threads_switch:
+        global read_discrete_inputs_data
+        global discrete_inputs_data
+        if discrete_inputs_data != read_discrete_inputs_data:
+            msg = str(read_discrete_inputs_data)
+            rc, mid = mqtt_client.publish("one2one/fpq", payload=msg, qos=1)  # qos
+            on_publish(msg, rc)
+            print(read_discrete_inputs_data)
+            discrete_inputs_data = read_discrete_inputs_data
+
 
 logging.basicConfig()
 log = logging.getLogger()
@@ -18,68 +105,8 @@ read_discrete_inputs_data = []
 discrete_inputs_data = []
 threads_switch = 1
 
-
-def modbus_rr():
-    global threads_switch
-    while threads_switch:
-        rr = modbus_client.read_discrete_inputs(0, 8, unit=0x01)
-        global read_discrete_inputs_data
-        read_discrete_inputs_data = rr.bits
-        modbus_client.write_coils(0, write_coils_data, unit=0x01)
-
-
-def modbus_print():
-    global threads_switch
-    while threads_switch:
-        global read_discrete_inputs_data
-        global discrete_inputs_data
-        if discrete_inputs_data != read_discrete_inputs_data:
-            msg = str(read_discrete_inputs_data)
-            rc, mid = mqtt_client.publish(topic, payload=msg, qos=1)  # qos
-            on_publish(msg, rc)
-            print(read_discrete_inputs_data)
-            discrete_inputs_data = read_discrete_inputs_data
-
-
-def on_publish(msg, rc):  # 成功发布消息的操作
-    if rc == 0:
-        print("publish success, msg = " + msg)
-
-
-def on_connect(client, userdata, flags, rc):  # 连接后的操作 0为成功
-    print("Connection returned " + str(rc))
-
-
-mqtt_client = mqtt.Client(
-    client_id="test_mqtt_sender_1",  # 用来标识设备的ID，用户可自己定义，在同一个实例下，每个实体设备需要有一个唯一的ID
-    clean_session=True,
-    userdata=None,
-    protocol='MQTTv311'
-)
-
-# trust = "E:\root_cert.pem"  # 开启TLS时的认证文件目录
-user = "ds11-01/zlq"
-pwd = "4MnBAZJy9XBqhbgrk05F1PFjS718d8PFVlr+jqqLo9o="
-endpoint = "ds11-01.mqtt.iot.gz.baidubce.com"
-port = 1883
-topic = "one2one/zlq"
-
-# client.tls_insecure_set(True)  # 检查hostname的cert认证
-# client.tls_set(trust)  # 设置认证文件
-mqtt_client.username_pw_set(user, pwd)  # 设置用户名，密码
-mqtt_client.connect(endpoint, port, 60)  # 连接服务 keepalive=60
-mqtt_client.on_connect = on_connect  # 连接后的操作
-mqtt_client.loop_start()
-time.sleep(2)
-'''
-11
-count = 0
-while count < 5:  # 发布五条消息
-    count = count + 1
-    msg = str(datetime.datetime.now())
-    rc, mid = client.publish(topic, payload=msg, qos=1)  # qos
-    on_publish(msg, rc)
-    time.sleep(2)
+while True:
+    time.sleep(1)
 '''
 threads = []
 t1 = threading.Thread(target=modbus_rr)
@@ -97,90 +124,24 @@ for i in range(60):
 
 threads_switch = 0
 modbus_client.close()
+'''
+'''
+0: Connection successful 1: Connection refused - incorrect protocol version
+2: Connection refused - invalid client identifier 3: Connection refused - server unavailable
+4: Connection refused - bad username or password 5: Connection refused - not authorised
+6-255: Currently unused.
+'''
 
 
 '''
-
-test = []
-while True:
-    rr = modbus_client.read_discrete_inputs(0, 8, unit=0x05)
-    if rr.bits != test:
-        print(rr.bits)
-        msg = str(rr.bits)
-        rc, mid = mqtt_client.publish(topic, payload=msg, qos=1)  # qos
-        test = rr.bits
-    # time.sleep(0.5)
-
-rr = modbus_client.read_discrete_inputs(0, 8, unit=0x05)
-
-
-rq = modbus_client.write_coils(0, [True]*8, unit=0x01)
-
-rr = modbus_client.read_coils(0, 8, unit=0x02)
-
-
-#---------------------------------------------------------------------------#
-# specify slave to query
-#---------------------------------------------------------------------------#
-# The slave to query is specified in an optional parameter for each
-# individual request. This can be done by specifying the `unit` parameter
-# which defaults to `0x00`
-#---------------------------------------------------------------------------#
-rr = modbus_client.read_coils(1, 1, unit=0x02)
-
-
-
-#---------------------------------------------------------------------------#
-# example requests
-#---------------------------------------------------------------------------#
-# simply call the methods that you would like to use. An example session
-# is displayed below along with some assert checks. Note that some modbus
-# implementations differentiate holding/input discrete/coils and as such
-# you will not be able to write to these, therefore the starting values
-# are not known to these tests. Furthermore, some use the same memory
-# blocks for the two sets, so a change to one is a change to the other.
-# Keep both of these cases in mind when testing as the following will
-# _only_ pass with the supplied async modbus server (script supplied).
-#---------------------------------------------------------------------------#
-rq = modbus_client.write_coil(6, True)
-rr = modbus_client.read_coils(6,1)
-assert(rq.function_code < 0x80)     # test that we are not an error
-assert(rr.bits[0] == True)          # test the expected value
-
-rq = modbus_client.write_coils(1, [True]*8)
-rr = modbus_client.read_coils(1,8)
-assert(rq.function_code < 0x80)     # test that we are not an error
-assert(rr.bits == [True]*8)         # test the expected value
-
-rq = modbus_client.write_coils(1, [False]*8)
-rr = modbus_client.read_discrete_inputs(1,8)
-assert(rq.function_code < 0x80)     # test that we are not an error
-assert(rr.bits == [False]*8)         # test the expected value
-
-rq = modbus_client.write_register(1, 10)
-rr = modbus_client.read_holding_registers(1,1)
-assert(rq.function_code < 0x80)     # test that we are not an error
-assert(rr.registers[0] == 10)       # test the expected value
-
-rq = modbus_client.write_registers(1, [10]*8)
-rr = modbus_client.read_input_registers(1,8)
-assert(rq.function_code < 0x80)     # test that we are not an error
-assert(rr.registers == [10]*8)      # test the expected value
-
-arguments = {
-    'read_address':    1,
-    'read_count':      8,
-    'write_address':   1,
-    'write_registers': [20]*8,
-}
-rq = modbus_client.readwrite_registers(**arguments)
-rr = modbus_client.read_input_registers(1,8)
-assert(rq.function_code < 0x80)     # test that we are not an error
-assert(rq.registers == [20]*8)      # test the expected value
-assert(rr.registers == [20]*8)      # test the expected value
-
-#---------------------------------------------------------------------------#
-# close the client
-#---------------------------------------------------------------------------#
-modbus_client.close()
+11
+count = 0
+while count < 5:  # 发布五条消息
+    count = count + 1
+    msg = str(datetime.datetime.now())
+    rc, mid = client.publish(topic, payload=msg, qos=1)  # qos
+    on_publish(msg, rc)
+    time.sleep(2)
 '''
+
+
